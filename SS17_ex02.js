ce4e1ae5f308ffe108e101ecf191e2a816aa4ca6
phamvanthe94 <mihthe05121994@gmail.com>
2025-08-14 11:46:49 +0900

    Nộp bài tập SS17_ex02

---
+++ SS17_ex02.js
@@ -0,0 +1,14 @@
+let body = document.getElementById(`body`);
+let btn = document.getElementById(`btn`);
+
+// console.log(body);
+// console.log(btn);
+
+// body.classList.add(`body`);
+
+body.addEventListener(`click`, function () {
+  body.classList.toggle(`body`);
+});
+
+// classList.toggle
+// bật tắt Css thêm vào Element
